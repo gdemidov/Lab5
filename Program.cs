3b0dc120835f65fc239d631de4b43b13a8471c8f
demidov_g@mail.ru <demidov_g@mail.ru>
2021-10-27 08:30:58 +0300

    1

--- Program.cs
+++ Program.cs
@@ -10,6 +10,20 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            int[] array = new int[7];
+            Random random = new Random();
+            int S = 0;
+            int ms;
+            for (int i = 0; i < 7; i++)
+            {
+                array[i] = random.Next(0, 10);
+                Console.Write("{0} ", array[i]);
+                S += array[i];
+            }
+            ms = S / 7;
+            Console.WriteLine();
+            Console.WriteLine("среднее арифмитическое = {0}", ms);
+            Console.ReadKey();
         }
     }
 }
